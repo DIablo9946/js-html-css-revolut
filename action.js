b8c68c8956691897596d53b46dae119f21e89406
Diablo9946 <diabloch9946@gmail.com>
2019-08-31 05:41:02 +0200

    Working but not separately.

---
+++ action.js
@@ -0,0 +1,23 @@
+// EX DI OGGI
+// NOME REPO= js-html-css-revolut
+// DESCRIZIONE=
+// Ricreare almeno l’header completo con i dropdown che al click
+// si mostrano e spariscono ricliccando.
+// La traccia da riprodurre è questa: https://www.revolut.com/en-IT/
+// Poi tutto quello che riuscite a ricreare della pagina è tutto di
+//  guadagnato per ripasso e ex ulteriore vostro. Quindi più fate più vi conviene.
+// NOTE=
+// quindi una cartella del repo con il progetto, sul quale andate
+// avanti più che potete, ma che almeno ha la richiesta base.
+// revolut.comrevolut.com
+
+$(document).ready(function(){
+
+$(".right a").click(function(){
+  $("li > .dropdownMenu").toggleClass("active");
+})
+
+
+
+
+});
